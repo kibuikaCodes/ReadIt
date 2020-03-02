4d24ec9b598de8415d75b2c0f9cb48e6aaa34867
willyngash <willynganga024@gmail.com>
2020-03-02 12:53:23 +0300

    Changed the menu icon on all pages

--- src/elements/DrawerAuth.js
+++ src/elements/DrawerAuth.js
@@ -78,7 +78,7 @@ export default function DrawerComponentAuth() {
 
   return (
     <div>
-      <Button onClick={toggleDrawer('right', true)} style={{color: 'blue', fontWeight: "bold"}}><img src="https://img.icons8.com/clouds/100/000000/menu.png" alt="menu icon" /></Button>
+      <Button onClick={toggleDrawer('right', true)} style={{color: 'black', fontWeight: "bold"}}><img src="https://img.icons8.com/clouds/50/000000/menu.png" alt="menu icon" /></Button>
       <Drawer anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
         {sideList('right')}
       </Drawer>
